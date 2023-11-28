1cf59896a96800d59268ca3a0cc1298bf2601520
G4briel25 <warmerishoth@gmail.com>
2023-11-28 11:28:26 -0400

    update no nome do desenvolvedor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const Page = () => {
   return (
     <>
       <Base/>
-      <p className="text-center text-white text-sm">Criado por Gabriel Jaune</p>
+      <p className="text-center text-white text-sm pt-5 pb-4 md:p-0">Criado por Gabriel Jaune</p>
     </>
   );
 };
--- src/components/Base.tsx
+++ src/components/Base.tsx
@@ -2,7 +2,7 @@ import { Section } from "./Section";
 
 export const Base = () => {
     return (
-        <div className="md:my-20 flex items-center justify-center">
+        <div className="md:mt-24 md:mb-16 flex items-center justify-center">
         
             <div className="container max-w-5xl bg-white m-3.5 p-7 rounded-lg shadow-gray-900 shadow-xl">
             <p className="my-4 md:my-8 text-xl md:text-2xl text-center font-bold">
